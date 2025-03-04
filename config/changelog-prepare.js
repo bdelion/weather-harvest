b25ae71a6ebc019f28e15cb194c666f4fd0737ba
Bertrand DELION <bertrand.delion@free.fr>
2025-03-04 10:08:15 +0100

    :poop: review changelog template

--- config/changelog-prepare.js
+++ config/changelog-prepare.js
@@ -1,10 +1,16 @@
 module.exports = (releaseNotes, context) => {
+  // Exclude merge commits from release notes
+  const filteredReleaseNotes = releaseNotes
+    .split('\n')
+    .filter(line => !line.includes('Merge pull request') && !line.includes('Merge branch'))
+    .join('\n');
+
   // Définir previousVersion à partir de la version précédente
   context.previousVersion = context.lastRelease.version || ''; // Si la dernière version n'existe pas, il sera vide
 
   // Vérifier si c'est la première version du projet
   context.isFirstVersion = context.nextRelease.version === '1.0.0';
 
-  // Retourner les notes de version sans modification (si vous avez une logique spécifique, vous pouvez la rajouter ici)
-  return releaseNotes;
+  // Retourner les notes de version filtrées
+  return filteredReleaseNotes;
 };
